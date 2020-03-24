ebcbf1ced095ae11166865c74e2611fa8ddcb388
supriyaBharti15 <supriya.simi@gmail.com>
2020-03-24 15:28:35 +0530

    my first commit

---
+++ app/src/main/java/com/example/fcmnotificationbg/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fcmnotificationbg;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fcmnotificationbg/webServices/WebServiceAPI.java
@@ -0,0 +1,5 @@
+package com.example.fcmnotificationbg.webServices;
+
+public interface WebServiceAPI {
+    //https://fcm.googleapis.com/fcm/send
+}
